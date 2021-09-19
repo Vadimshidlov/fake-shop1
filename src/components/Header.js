b11141a7083a7afe5b86abb54caa91ea126ba8f7
Vadimshidlov <mat23vadim@mail.ru>
2021-09-19 15:00:14 +0300

    Change name 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="ui fixed menu">
             <div className="ui container center">
-                <h2>MishkaFakeShop</h2>
+                <h2>MishkaFakeShopForMishut</h2>
             </div>
         </div>
     );
