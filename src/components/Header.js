654d6a25ca68078b8a173680fdb174069c081df8
Vadimshidlov <mat23vadim@mail.ru>
2021-09-19 15:18:11 +0300

    Add description

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ const Header = () => {
         <div className="ui fixed menu">
             <div className="ui container center">
                 <h2>MishkaFakeShopForMishut</h2>
+                <h4>Мишка, это для тебя
+                    <br/>
+                    Написал такой маленький, учебный магазинчик!)
+                </h4>
             </div>
         </div>
     );
